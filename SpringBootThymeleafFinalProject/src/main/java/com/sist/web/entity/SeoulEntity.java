4b5a6d649b64b26a971a52b7c3cb51ff55a68b6a
minhyeok08 <ssklas08@gmail.com>
2023-08-31 18:06:13 +0900

    thymeleaf 이용 검색,페이징,목록출력

---
+++ SpringBootThymeleafFinalProject/src/main/java/com/sist/web/entity/SeoulEntity.java
@@ -0,0 +1,19 @@
+package com.sist.web.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="seoul_location")
+@Getter
+@Setter
+public class SeoulEntity {
+	@Id
+	private int no;
+	private int hit;
+	private String title, poster, msg, address;
+}
